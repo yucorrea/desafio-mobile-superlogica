7842ddd5084cec9158ab63b56ddeda5f15aed29c
Yuri Correa <yuri.correa1@outlook.com>
2021-09-17 15:06:50 -0300

    feat: add bottomTab navigator

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,24 +1,81 @@
 import React from 'react';
-import { createNativeStackNavigator, NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import Home from './views/Home'
 import Episode from './views/Episode'
-import EpisodeCharacter, { RouteProps } from './views/EpisodeCharacter'
+import EpisodeCharacter from './views/EpisodeCharacter'
+
+import { Feather } from "@expo/vector-icons";
+import theme from './global/styles/theme';
+import { View } from 'react-native';
 
 const Stack = createNativeStackNavigator();
+const BottomTab = createBottomTabNavigator();
 
-const MainRoute = () => {
+
+const icons = {
+  Home: {
+    lib: Feather,
+    name: "home"
+  },
+  Episodes: {
+    lib: Feather,
+    name: "play"
+  },
+  Locations: {
+    lib: Feather,
+    name: "map-pin"
+  }
+}
+
+const HomeStack = () => {
   return (
-    <Stack.Navigator >
+    <Stack.Navigator>
       <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
       <Stack.Screen name="Episodes" component={Episode} options={{ headerShown: true }} />
       <Stack.Screen
         name="EpisodeCharacter" component={EpisodeCharacter}
-        options={({ route } : any) => ({
+        options={({ route }: any) => ({
           title: route.params.episode
         })}
       />
     </Stack.Navigator>
+  )
+}
+
+const MainRoute = () => {
+  return (
+    <BottomTab.Navigator screenOptions={({ route }) => ({
+      tabBarIcon: ({ color, size }) => {
+
+        //@ts-ignore
+        const { lib: Icon, name } = icons[route.name];
+
+        return <Icon name={name} size={size} color={color} />
+      },
+      tabBarActiveTintColor: theme.colors.primary,
+      tabBarInactiveTintColor: theme.colors.text,
+    })}
+
+    >
+      <BottomTab.Screen
+        name="Home"
+        component={HomeStack}
+        options={{ title: "Personagens", headerShown: false }}
+      />
+
+      <BottomTab.Screen name="Episodes"
+        component={View}
+        options={{ title: "Episódios" }}
+      />
+
+      <BottomTab.Screen
+        name="Locations"
+        component={View}
+        options={{ title: "Localizações" }}
+      />
+    </BottomTab.Navigator>
   );
 }
 
