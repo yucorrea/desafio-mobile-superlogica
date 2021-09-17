06061ec16223794ad6c7776d046f59e811f99ec5
Yuri Correa <yuri.correa1@outlook.com>
2021-09-17 02:13:30 -0300

    feat: adjust App.tsx

--- App.tsx
+++ App.tsx
@@ -2,17 +2,45 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import {Provider} from 'react-redux';
+import { ThemeProvider } from 'styled-components/native';
+import AppLoading from 'expo-app-loading';
+
+import { 
+  useFonts, 
+  Inter_300Light,
+  Inter_400Regular, 
+  Inter_600SemiBold 
+} from '@expo-google-fonts/inter';
 
 import MainRoute from './src/routes';
-import { Store } from './src/redux/store';
+import { Persistor, Store } from './src/redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+
+import theme from './src/global/styles/theme';
 
 export default function App() {
+
+  let [fontsLoaded] = useFonts({
+    Inter_300Light,
+    Inter_400Regular, 
+    Inter_600SemiBold 
+  });
+
+  if(!fontsLoaded) {
+    return <AppLoading />
+  }
+
   return (
     <Provider store={Store}>
-    <NavigationContainer>
-      <StatusBar style="auto" />
-      <MainRoute />
-    </NavigationContainer>
+    <PersistGate loading={null} persistor={Persistor}>
+
+      <ThemeProvider theme={theme}>
+        <NavigationContainer>
+          <StatusBar style="auto" />
+          <MainRoute />
+        </NavigationContainer>
+      </ThemeProvider>
+    </PersistGate>
     </Provider>
   );
 }
