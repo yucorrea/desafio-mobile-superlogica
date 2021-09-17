b92ed743604dac60aa2ae9df75c285f1717af405
Yuri Correa <yuri.correa1@outlook.com>
2021-09-17 02:02:59 -0300

    style: create global styles

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,21 @@
+export default {
+    colors: {
+        primary: "#FF9800",
+        secondary: "#3C3E44",
+
+        background: "#24282F",
+        title: "#FFF",
+        text: "#9E9E9E",
+        title_dark: "#24282F", 
+
+        success: "#55CC44",
+        danger:   "#D63D2E",
+    },
+    fonts: {
+        light: "Inter_300Light",
+        regular: "Inter_400Regular",
+        bold: "Inter_600SemiBold"
+    }
+}
+
+ 
